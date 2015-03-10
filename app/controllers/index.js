99fa82bb06fb4490ed1b4a932a4c63129f744ff6
AndroidHell <ralkkai1337@gmail.com>
2015-03-10 18:15:29 -0500

    initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,53 @@
+// action bar goes here
+
+
+// red button 1
+$.redButton1.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#f00';
+}); 
+// green button 1
+$.greenButton1.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#690';
+});
+// blue button 1
+$.blueButton1.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#39f';
+});
+ //---------------------------------------------------
+ // red button 2
+$.redButton2.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#f00';
+});
+// green button 2
+$.greenButton2.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#690';
+});
+// blue button 2
+$.blueButton2.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#39f';
+});
+//---------------------------------------------------
+ // red button 3
+$.redButton3.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#f00';
+});
+// green button 3
+$.greenButton3.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#690';
+});
+// blue button 3
+$.blueButton3.addEventListener('click', function(e)
+{
+	$.index.backgroundColor = '#39f';
+});
+//--------------------------------------------------- 
+// open window
+$.index.open();
